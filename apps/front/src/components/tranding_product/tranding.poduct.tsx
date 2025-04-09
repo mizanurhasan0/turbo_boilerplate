76028668213ebee089778661747b781c56662c5b
mizanurhasan0 <sajal596@diu.edu.bd>
2025-04-09 18:32:30 +0600

    font changed

--- apps/front/src/components/tranding_product/tranding.poduct.tsx
+++ apps/front/src/components/tranding_product/tranding.poduct.tsx
@@ -5,8 +5,10 @@ import Card from '../cardList/card'
 export default function TrandingPoduct() {
     return (
         <div className="container mx-auto">
-            <h1>Tranding Products</h1>
-
+            <div className="text-center w-full mx-auto space-y-4">
+                <h1 className="font-semibold text-4xl">Tranding Products</h1>
+                <img src='./icons/separator.png' alt='separator' className="mx-auto" />
+            </div>
             <div>
                 <p>Featured</p>
             </div>
@@ -14,7 +16,7 @@ export default function TrandingPoduct() {
             <div className="grid grid-cols-2 md:grid-cols-4 gap-5">
                 {Array.from({ length: 10 }).map((d, i) => (
                     <div key={i}>
-                        <Card name={"this is product name"} price={100} sellingPrice='213213' />
+                        <Card name={"this is product name"} price={100} sellingPrice='213' />
                     </div>
                 ))}
             </div>
